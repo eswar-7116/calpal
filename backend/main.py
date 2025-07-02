0b6c3228eb4467c003e1cfcfee304daf03ed7e77
eswar-7116 <eswardudi06@gmail.com>
2025-07-02 23:13:01 +0530

    integrate agent

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,46 @@
-import time
+from dotenv import load_dotenv
 from fastapi import FastAPI
-from schema import ChatRequest
+from backend.agent.agent import graph, WorkflowState
+from langchain_core.messages import HumanMessage
+from langgraph.graph.message import add_messages
+from backend.schema import ChatRequest
+from fastapi.middleware.cors import CORSMiddleware
+
+# Load .env
+load_dotenv("./.env")
+
+# In-memory message store
+state = WorkflowState(messages=[])
 
 # Initialize app
 app = FastAPI()
 
-# POST /chat
+# Allow frontend to call
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.post("/chat")
 def chat(req: ChatRequest):
-    # Simulate loading
-    time.sleep(3)
-    
-    # Extract request data
+    global state
+
+    # Extract user message from request
     msg = req.chat
-    
-    # Return message
+
+    # Add new message
+    messages = add_messages(state.messages, [HumanMessage(content=msg)])
+    state = WorkflowState(messages=messages)
+
+    # Call LangGraph agent
+    result = graph.invoke(state)
+    state = WorkflowState(messages=result["messages"])
+
+    # Extract bot response
+    ai_reply = state.messages[-1].content
+
     return {
-        "message": msg
+        "message": ai_reply
     }
